1552a0021c40519b80f2d10645b71bedaaa28601
Sidath S Wijesooriya <sidath.s.wijesooriya@saint-gobain.com>
2022-03-01 00:05:20 -0500

    Added augmentations for CIFAR10 and MNIST.

--- main.py
+++ main.py
@@ -14,27 +14,78 @@ from models.model import BPNet
 from trainer.trainer import train, test
 from cmd import args
 
-# data
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    #transforms.Normalize((0.1307,), (0.3081,)) # MNIST stats
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) # CIFAR 10 stats
-    # TODO Sid 
-])
-batch_size = args.batch_size
-train_loader = torch.utils.data.DataLoader(
-    torchvision.datasets.CIFAR10('data', train=True, download=True, transform=transform),
-    batch_size=batch_size, shuffle=True
-)
-test_loader = torch.utils.data.DataLoader(
-    torchvision.datasets.CIFAR10('data', train=False, download=True, transform=transform),
-    batch_size=batch_size, shuffle=True
-)
+# data augmentation using transforms.Compose
+DSToUse = "CIFAR10"  # Or MNIST
+
+if DSToUse == "CIFAR10":
+    # CIFAR10 - (60000, 3, 32, 32)
+    transform = transforms.Compose(
+        [
+            transforms.ToPILImage(),
+            transforms.ColorJitter(brightness=0.5, hue=0.3),
+            transforms.RandomRotation(degrees=25),
+            transforms.RandomHorizontalFlip(
+                p=0.5
+            ),  # Random horizontal flip 50% of the time
+            transforms.RandomGrayscale(
+                p=0.1
+            ),  # Convert to grayscale randomly 10% of the time
+            transforms.ToTensor(),
+            # transforms.Normalize((0.1307,), (0.3081,)) # MNIST stats
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),  # CIFAR 10 stats
+        ]
+    )
+
+    batch_size = args.batch_size
+    train_loader = torch.utils.data.DataLoader(
+        torchvision.datasets.CIFAR10(
+            "data", train=True, download=True, transform=transform
+        ),
+        batch_size=batch_size,
+        shuffle=True,
+    )
+    test_loader = torch.utils.data.DataLoader(
+        torchvision.datasets.CIFAR10(
+            "data", train=False, download=True, transform=transform
+        ),
+        batch_size=batch_size,
+        shuffle=True,
+    )
+
+elif DSToUse == "MNIST":
+    # MNIST - (70000,1, 28,28)
+    transform = transforms.Compose(
+        [
+            transforms.ToPILImage(),
+            transforms.RandomRotation(degrees=25),
+            transforms.RandomPerspective(distortion_scale=0.5, p=0.5,),
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))  # MNIST stats
+            # transforms.Normalize((0.5, 0.5), (0.5, 0.5, 0.5)),  # CIFAR 10 stats
+        ]
+    )
+
+    batch_size = args.batch_size
+    train_loader = torch.utils.data.DataLoader(
+        torchvision.datasets.MNIST(
+            "data", train=True, download=True, transform=transform
+        ),
+        batch_size=batch_size,
+        shuffle=True,
+    )
+    test_loader = torch.utils.data.DataLoader(
+        torchvision.datasets.MNIST(
+            "data", train=False, download=True, transform=transform
+        ),
+        batch_size=batch_size,
+        shuffle=True,
+    )
+
 
 # model
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-#model = BPNet(28 * 28, 1, 2000, 10).to(device) # MNIST
-model = BPNet(32 * 32, 3, 2000, 10).to(device) # CIFAR 10 
+# model = BPNet(28 * 28, 1, 2000, 10).to(device) # MNIST
+model = BPNet(32 * 32, 3, 2000, 10).to(device)  # CIFAR 10
 
 # training
 E = args.epochs
@@ -49,7 +100,7 @@ while True:
         os.makedirs(run_base_dir)
         break
     i += 1
-with open(run_base_dir / "args.json", 'w') as f:
+with open(run_base_dir / "args.json", "w") as f:
     json.dump(vars(args), f)
 train_writer = SummaryWriter(run_base_dir / "train")
 test_writer = SummaryWriter(run_base_dir / "test")
@@ -57,10 +108,18 @@ test_writer = SummaryWriter(run_base_dir / "test")
 # main training loop
 for epoch in tqdm(range(E), desc="Epoch", total=args.epochs, dynamic_ncols=True):
     # train
-    train(model, train_loader, optimizer, nn.CrossEntropyLoss(), device, train_writer, epoch) 
+    train(
+        model,
+        train_loader,
+        optimizer,
+        nn.CrossEntropyLoss(),
+        device,
+        train_writer,
+        epoch,
+    )
     # test
-    test(model, test_loader, nn.CrossEntropyLoss(), device, test_writer, epoch) 
+    test(model, test_loader, nn.CrossEntropyLoss(), device, test_writer, epoch)
     # save checkpoint
     if epoch % 10 == 9:
-        torch.save(model.state_dict(), run_base_dir / f"epoch_{epoch+1}.pt") 
-torch.save(model.state_dict(), run_base_dir / f"epoch_{epoch+1}.pt") 
+        torch.save(model.state_dict(), run_base_dir / f"epoch_{epoch+1}.pt")
+torch.save(model.state_dict(), run_base_dir / f"epoch_{epoch+1}.pt")
